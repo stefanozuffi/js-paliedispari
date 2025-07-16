557e9c020715307111fffa2c23801197039ccb51
stefanozuffi <stefano.zuffi06@outlook.com>
2025-07-16 15:01:46 +0200

    aggiunto delle correzioni alle dichiarazioni delle variabili + ho sperimentato con while loop per gestir input erronei dell'utente

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Funzione Palindromo
 
 //Chiediamo all'utente una parola e salviamo il suo input nella variabile string
-input_1 = prompt("Inserisci una parola:")
+let input_1 = prompt("Inserisci una parola:")
 
 function is_palindrome(string) {
     //Vogliamo che la stringa in input venga invertita in inverse_string
@@ -32,22 +32,32 @@ else {
 // Gioco Pari o Dispari
 
 //Chiediamo all'utente di scegliere pari o dispari
-input_2 = prompt('Scegli pari o dispari:').toLowerCase()
+let input_2 = prompt('Scegli pari o dispari:').toLowerCase()
+while (input_2.toLowerCase() !== 'pari' && input_2.toLowerCase() !== 'dispari') {
+    input_2 = prompt('Scegli pari o dispari:').toLowerCase()
+}
 
 console.log(`Hai scelto ${input_2}`)
 
+
 //Poi di scegliere un numero e convertiamo la sua stringa di risposta in un numero:
-user_num = Number(prompt("Scegli un numero da 1 a 5"))
+let user_num = Number(prompt("Scegli un numero da 1 a 5"))
+while (!(user_num <= 5 && user_num >= 1)) {
+    user_num = Number(prompt("Scegli un numero da 1 a 5"))
+}
+
+console.log(`Hai scelto il numero: ${user_num}`)
+
 
 //Definiamo una funzione per generare un numero tra 1 e 5
 function gen_random(min,max) {
    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-computer_num = gen_random(1,5)
+const computer_num = gen_random(1,5)
 console.log(`Il computer ha scelto ${computer_num}`)
 
-final_sum = user_num + computer_num
+const final_sum = user_num + computer_num
  
 //Ora calcoliamo chi ha vinto, a seconda di input_2 e il valore di final_sum
 function is_odd_or_even(num) {
@@ -61,7 +71,7 @@ function is_odd_or_even(num) {
     return res
 }
 
-sum_type = is_odd_or_even(final_sum)
+const sum_type = is_odd_or_even(final_sum)
 console.log(`La somma di ${user_num} e ${computer_num} fa ${final_sum}, che è ${sum_type}`)
 
 //Annunciamo il risultato in console
